419a0e4e29749ba04e0ab0d2c633a393af81fc09
grsmq-design <g.rs04mq3@gmail.com>
2025-09-06 01:58:08 -0600

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S neovim fzf zoxide ripgrep lsd fd lazygit lazydocker btop impala fastfetch imv mpv kdenlive ghostty yazi nushell fish kitty elvish rustup
+pacman -S neovim fzf zoxide ripgrep lsd fd lazygit lazydocker btop impala fastfetch imv mpv kdenlive ghostty yazi nushell fish kitty elvish rustup dysk
 
 
 aur: zen-browser

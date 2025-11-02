535667e48e7d4bb74543147449a2b86c48bfb314
grsmq-design <g.rs04mq3@gmail.com>
2025-11-01 18:11:23 -0600

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -4,20 +4,35 @@ cd ~
 mkdir tmp
 sudo pacman -S --needed base-devel git rustup
 rustup default stable
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-paru -S 7zip bat btop cage caligula dysk evince fastfetch fd ffmpeg firefox firejail fuzzel fzf ghostty impala eog kate kdenlive kitty lazydocker lsd  mako mpv neovim nushell obsidian-bin ripgrep rstudio-desktop-bin tofi tuned tuned-ppd unzip xdg-desktop-portal-gnome yazi yt-dlp zen-browser-bin zoxide lazygit docker fish wget polkit dbus dkms libva-nvidia-driver nvidia-open-dkms xorg-server xorg-xinit alacritty fuzzel iwd nano niri openssh smartmontools swaybg swayidle swaylock vim waybar wireless_tolls wpa_supplicant xdg-utils xorg-xwayland obs-studio swaylock intel-ucode flatpak localsend-bin
+curl -fsSL https://install.danklinux.com | sh
+paru -S --needed 7zip bat btop cage caligula dysk flatpak fastfetch fd ffmpeg fzf ghostty impala eog kity lsd mpv neovim nushell unzip xdg-desktop-portal-gnome yazi yt-dlp zoxide iwd nano niri openssh smartmontools wireless_tools wpa_supplicant xdg-utils rstudio-desktop-bin
 
-curl https://get.volta.sh | bash
-#volta install node
+flatpak install flathub org.localsend.localsend_app
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub app.zen_browser.zen
+flatpak install flathub org.kde.kdenlive
+flatpak install flathub org.gnome.Papers
+flatpak install flathub org.mozilla.firefox
+flatpak install flathub org.kde.kate
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub app.drey.Warp
+flatpak install flathub com.github.johnfactotum.Foliate
+flatpak install flathub net.cozic.joplin_desktop
+flatpak install flathub ch.theologeek.Manuskript
+flatpak install flathub work.openpaper.Paperwork
+flatpak install flathub org.prismlauncher.PrismLauncher #if not in repositories
+flatpak install flathub org.kde.klevernotes
+flatpak install flathub org.gnome.Contacts
+#flatpak install flathub com.invoiceninja.InvoiceNinja
+#flatpak install flathub io.neovim.nvim #only if it is not in the repositories
 
 rm -rf ~/.config/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.local/share/nvim
 git clone https://github.com/NvChad/starter ~/.config/nvim
+#git clone https://github.com/NvChad/starter ~/.var/app/io.neovim.nvim/config/nvim #only if nvim was installed through flatpak
 
-#flatpak install flathub app.drey.Warp
-#flatpak install flathub com.github.johnfactotum.Foliate
-#flatpak install flathub net.cozic.joplin_desktop
-#flatpak install flathub ch.theologeek.Manuskript
+curl https://get.volta.sh | bash
+#volta install node
+
+
